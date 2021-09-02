d3231bd2883d8b5589747395c3a4b4668e4a118f
Toggle <toggledy@gmail.com>
2021-09-02 21:34:17 +1000

    hmmmmmm

--- test/dbenv.ts
+++ test/dbenv.ts
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
+import { exec } from 'child-process-promise'  
+const { parse } = require('pg-connection-string')
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
-import { PrismaClient } from '@prisma/client'
 
 // const SUPABASE_URL = 'https://emiwtmbwvmyoxyxxlwbv.supabase.co'
 // const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDQwNTM0NiwiZXhwIjoxOTQ1OTgxMzQ2fQ.F4oJH0B3Zugi7TZcq5solrKrNg_8lAp9ZHhsZ2jtFAg'
@@ -13,9 +16,41 @@ import { PrismaClient } from '@prisma/client'
 
 const SUPABASE_URL = "http://localhost:9001"
 const SUPABASE_ANON_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoiYW5vbiJ9.36fUebxgx1mcBo4s19v0SzqmzunP--hm_hep0uLX0ew"
+
 export type Client = { supabase: SupabaseClient }
 
-const prisma = new PrismaClient()
+let dbenvReady: { asUser: Client, asAdmin: Client } | null = null
+export async function dbenv() {
+  if (dbenvReady) return dbenvReady
+
+  const testdb = await setupTestDb()
+  const asUser = await getUserClient()
+  const asAdmin = await getAdminClient()
+
+  dbenvReady = { db, asUser, asAdmin }
+  return dbenvReady
+}
+
+/** Prepare a separate testing database to run tests on */
+async function setupTestDb() {
+  const realDatabaseUrl = process.env.DATABASE_URL
+  await exec(`psql ${realDatabaseUrl} -c "drop database if exists postgres;"`)
+  await exec(`psql ${realDatabaseUrl} -c "create database postgres;"`)
+
+  const { user, password, host, port } = parse(realDatabaseUrl)
+  const testdb = `postgres://${user}:${password}@${host}:${port}/postgres`
+
+  // console.info("Installing supabase schema...")
+  await exec(`psql ${testdb} -f sql/supabase.sql`)
+
+  // console.info("Installing prisma schema...")
+  await exec(`DATABASE_URL=${testdb} npm run migrate up`)
+
+  // console.info("Applying policies.sql...")
+  await exec(`psql ${testdb} -f sql/policies.sql`)
+
+  return testdb
+}
 
 async function getAdminClient() {
   const asAdmin = { supabase: createClient(SUPABASE_URL, SUPABASE_ANON_KEY) }
@@ -27,19 +62,12 @@ async function getAdminClient() {
   return asAdmin
 }
 
-let dbenvCache: { asUser: Client, asAdmin: Client, prisma: PrismaClient } | null = null
-export async function dbenv() {
-  if (dbenvCache) return dbenvCache
-
+async function getUserClient() {
   const asUser = { supabase: createClient(SUPABASE_URL, SUPABASE_ANON_KEY) }
   const { error, data } = await asUser.supabase.auth.signIn({ email: "testuser@example.com", password: "testuser-waffles" })
   if (error?.message === "Invalid login credentials") {
     const { error, data } = await asUser.supabase.auth.signUp({ email: "testuser@example.com", password: "testuser-waffles" })
     expect(error).toBe(null)
   }
-
-  const asAdmin = await getAdminClient()
-
-  dbenvCache = { asUser, asAdmin, prisma }
-  return dbenvCache
+  return asUser
 }
--- test/site.test.ts
+++ test/site.test.ts
@@ -1,14 +1,8 @@
-import { dbenv } from './helpers'
+import { dbenv } from './dbenv'
 
 test('pattern access rules', async () => {
-  const { prisma, asUser, asAdmin } = await dbenv()
+  const { db, asUser, asAdmin } = await dbenv()
 
-  await prisma.patterns.create({
-    data: {
-      title: "die, der, das",
-      slug: "die-der-das"
-    }
-  })
 
   const { error, data } = await asUser.supabase.from("patterns").select()
   expect(error).toBe(null)
