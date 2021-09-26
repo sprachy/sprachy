a1fab730f26ff40030176af8ba12e8fe30dad505
Toggle <toggledy@gmail.com>
2021-09-26 20:00:29 +1000

    Test env cleanup

--- test/dbenv.ts
+++ test/dbenv.ts
@@ -1,17 +1,7 @@
 require('dotenv').config()
 
-import { exec } from 'child-process-promise'  
 import { createClient, Session, SupabaseClient } from '@supabase/supabase-js'
 import { UserAPI, AdminAPI } from '../src/api'
-// const SUPABASE_URL = 'https://emiwtmbwvmyoxyxxlwbv.supabase.co'
-// const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDQwNTM0NiwiZXhwIjoxOTQ1OTgxMzQ2fQ.F4oJH0B3Zugi7TZcq5solrKrNg_8lAp9ZHhsZ2jtFAg'
-
-
-// Supabase URL: http://localhost:8000
-// Supabase Key (anon, public): eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoiYW5vbiJ9.36fUebxgx1mcBo4s19v0SzqmzunP--hm_hep0uLX0ew
-// Supabase Key (service_role, private): eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoic2VydmljZV9yb2xlIn0.necIJaiP7X2T2QjGeV-FhpkizcNTX8HjDDBAxpgQTEI
-// Database URL: postgres://postgres:postgres@localhost:5432/postgres
-// Email testing interface URL: http://localhost:9000
 
 export type UserClient = { api: UserAPI, db: SupabaseClient, session: Session }
 export type AdminClient = { adminApi: AdminAPI, api: UserAPI, db: SupabaseClient, session: Session }
@@ -19,7 +9,6 @@ export type ServiceClient = { db: SupabaseClient }
 
 type TestEnvironment = { asService: ServiceClient, asUser: UserClient, asAdmin: UserClient } 
 let dbenvReady: TestEnvironment | null = null
-let testenvPromise: Promise<TestEnvironment> | null = null
 
 export async function dbenv() {
   if (dbenvReady) return dbenvReady
--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -1,6 +1,6 @@
 require('dotenv').config()
 import { exec } from 'child-process-promise'  
-import { createClient, Session, SupabaseClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 import { TEST_USER_EMAIL, TEST_USER_PASSWORD, TEST_ADMIN_EMAIL, TEST_ADMIN_PASSWORD } from './constants'
 
 async function makeAccount(email: string, password: string) {
@@ -24,23 +24,24 @@ export default async function globalSetup() {
 
   // Drop and recreate public schema
   // This leaves the supabase "auth" schema unchanged between tests
-  await exec(`psql ${testdb} -c "drop schema if exists public cascade;"`)
-  await exec(`psql ${testdb} -c "create schema public;"`)
+  await exec(`echo "drop schema if exists public cascade; create schema public;" | psql ${testdb}`)
 
   // Add base supabase schema
   await exec(`psql ${testdb} -f sql/supabase.sql`)
 
+  // Run migrations
   await exec(`DATABASE_URL=${testdb} npm run migrate up`)
 
-  await exec(`psql ${testdb} -f sql/policies.sql`)
-  // It's mean to be idempotent, run it twice to test that
-  await exec(`psql ${testdb} -f sql/policies.sql`)
+  // It's mean to be idempotent, so run policies.sql twice to test that
+  await exec(`psql ${testdb} -f sql/policies.sql && psql ${testdb} -f sql/policies.sql`)
 
-  // Create test user account
-  await makeAccount(TEST_USER_EMAIL, TEST_USER_PASSWORD)
+  // Create test accounts
+  const [user, adminUser] = await Promise.all([
+    makeAccount(TEST_USER_EMAIL, TEST_USER_PASSWORD),
+    makeAccount(TEST_ADMIN_EMAIL, TEST_ADMIN_PASSWORD)
+  ])
 
-  // Create test admin account
-  const adminUser = await makeAccount(TEST_ADMIN_EMAIL, TEST_ADMIN_PASSWORD)
+  // Make admin user an admin
   const db = createClient(process.env.TEST_SUPABASE_URL!, process.env.TEST_SUPABASE_SECRET_KEY!)
   await db.from("accounts").update({ is_admin: true }).match({ id: adminUser.id })
 }
