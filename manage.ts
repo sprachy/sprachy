78e65e7ded263540c8248465123181be1881b8f6
Toggle <toggledy@gmail.com>
2021-10-11 19:20:56 +1100

    Niceish manage.ts shell

--- manage.ts
+++ manage.ts
@@ -4,6 +4,38 @@ import dotenv from 'dotenv'
 dotenv.config()
 import sade from 'sade'
 import { db } from './server/db'
+import repl from 'repl'
+const vm = require("vm");
+const { processTopLevelAwait } = require("node-repl-await");
+
+function isRecoverableError(error) {
+    if (error.name === 'SyntaxError') {
+        return /^(Unexpected end of input|Unexpected token)/.test(error.message);
+    }
+    return false;
+}
+
+async function myEval(code, context, filename, callback) {
+    code = processTopLevelAwait(code) || code;
+
+    try {
+        let result = await vm.runInNewContext(code, context);
+        callback(null, result);
+    } catch (e) {
+        if (isRecoverableError(e)) {
+            callback(new repl.Recoverable(e));
+        } else {
+            console.log(e);
+        }
+    }
+}
+
+let shelling: boolean = false
+async function shell() {
+  shelling = true
+  const r = repl.start({ prompt: "> ", eval: myEval })
+  r.context.db = db
+}
 
 async function adminify(email: string) {
   const user = await db.users.getByEmail(email)
@@ -16,11 +48,15 @@ async function adminify(email: string) {
 }
 
 process.on('unhandledRejection', (reason) => {
+  if (shelling) return
   console.error(reason)
   process.exit(-1)
 })
 
 sade('manage.ts')
+  .command('shell')
+    .describe('Open a node REPL in the app context')
+    .action(shell)
   .command('adminify <email>')
     .describe('Make the user with the given email an admin')
     .action(adminify)
