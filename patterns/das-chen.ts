62b7a72690453464812a411f41df69976bfead54
Toggle <toggledy@gmail.com>
2021-12-16 06:07:54 +1100

    Level 1 exercises for das-chen

--- patterns/das-chen.ts
+++ patterns/das-chen.ts
@@ -18,11 +18,22 @@ der Pantoffel, das Tier / das Pantoffeltierchen
 
 Do you see the pattern? These kinds of diminutives are always neuter, no matter what gender the base word originally had! So even if you haven't seen a specific word before, you know you can use _das_ if it ends in _-chen_.
 
+Diminutives can also end in _-lein_, which is similarly always _das_, but this is much less common than _-chen_ in modern German. Words ending in _-lein_ tend to sound pretty old-fashioned.
+
 <Tip title="Smol prevalence">
 Some nouns are used mainly in their diminutive form. For example, _das Eichhörnchen_ is more common than _das Eichhorn_. _das Mädchen_ refers to a young girl, while the root _die Magd_ (the maid) would sound very archaic in comparison.
 </Tip>
   `,
   exercises: [
+    /**
+     * What is the user practicing here?
+     *
+     * - chen words are always das
+     * - combined with prior patterns:
+     *    - using the right gendered form of ein/mein/etc
+     *    - plural overrides the das
+     */
+
     // LEVEL 1
 
     [
@@ -37,6 +48,23 @@ Some nouns are used mainly in their diminutive form. For example, _das Eichhörn
           "[Dein] Brötchen ist zum Brennstoff für das ewige Feuer meiner Seele geworden.",
         translation:
           "[Your] bread roll has become fuel for the eternal fire of my soul.",
+      },
+      {
+        from: "lukas",
+        message: "Und was ist mit [meine] Haselnüsschen?",
+        translation: "And what about [my] little hazelnuts?",
+      },
+      {
+        from: "squirrel",
+        message:
+          "[Deine] Haselnüsschen funkeln in den Weiten des Gedankenraums.",
+        translation:
+          "[Your] little hazelnuts sparkle in the far reaches of mindspace.",
+      },
+      {
+        from: "squirrel",
+        message: "Jetzt werde ich [ein] Nickerchen machen. Gute Nacht.",
+        translation: "Now I will have [a] nap. Good night.",
       },
     ],
 
