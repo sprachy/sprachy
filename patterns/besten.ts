6487e55fb828a529673d57400ae7d9a63dad98ed
Two-Kay <lg2711@hotmail.de>
2022-01-21 00:41:54 +0100

    Level 2 Superlative

--- patterns/besten.ts
+++ patterns/besten.ts
@@ -24,8 +24,40 @@ However the superlative would never appear on its own unlike the comparative, th
 You would commonly see the word _am_ like in the phrase _am besten_ placed infront of a superlative which is similar to the _the_ in _the best_. Although you may also use an article infront of the superlative if you're explicitly referring to someone or something, keep in mind the changes to the adjective endings then.
 </Tip>`,
   stories: [
-
     // LEVEL 1
+    {
+      lines: [
+        {
+          from: "robot",
+          message: "Am [besten] wäre es für Sie wenn Sie den Fisch gehorchen würden.",
+          translation: "It would be [best] for you if you obeyed the fish.",
+          hint: "[gut]",
+        },
+        {
+          from: "explorer",
+          message: "Der Fisch schien wohl nicht das [neutraliste] Jury-Mitglied zu sein...",
+          translation: "I guess the fish didn't seem to be the most [neutral] jury member....",
+        },
+        {
+          from: "fish",
+          message: "*blub!*",
+          translation: "*blub!*",
+        },
+        {
+          from: "explorer",
+          message: "Ich glaube ich habe [besseres] zu tun.",
+          translation: "I think I have [better] things to do.",
+          hint: "[comparative]",
+        },
+        {
+          from: "explorer",
+          message: "Adieu!",
+          translation: "Adieu!",
+        }
+      ]
+    },
+
+    // LEVEL 2
     {
       lines: [
         {
