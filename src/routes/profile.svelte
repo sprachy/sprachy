99329f1d7f7c5d436442b6e6e0f8b769ab761a00
Two-Kay <lg2711@hotmail.de>
2022-04-11 23:03:18 +0200

    Initialized profile settings page

---
+++ src/routes/profile.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import sprachy from "$lib/sprachy"
+  import SiteLayout from "$lib/SiteLayout.svelte"
+  const { spa, api } = sprachy.expectSPA()
+
+  let errors: Record<string, string> = {}
+
+  const params = new URLSearchParams(window.location.search)
+</script>
+
+<SiteLayout>
+  <h1>Profile Settings</h1>
+</SiteLayout>
