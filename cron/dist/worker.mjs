005d3290989c9f942e5dad97f60c38603853863b
Toggle <toggledy@gmail.com>
2022-05-11 05:53:45 +1000

    Match deployed cron worker

--- cron/dist/worker.mjs
+++ cron/dist/worker.mjs
@@ -1,5 +1,5 @@
 export default {
   async scheduled(event, env, ctx) {
-    ctx.waitUntil(env.sprachy.fetch("/heartbeat"))
-  },
+    ctx.waitUntil(env.sprachy.fetch("https://sprachy.com/heartbeat"))
+  }
 }
