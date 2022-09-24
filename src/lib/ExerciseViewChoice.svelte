a56bf93e84775da6d41a1124d4f1904af4c77569
Toggle <toggledy@gmail.com>
2022-09-25 07:31:55 +1000

    Break Choices UI out into its own component

--- src/lib/ExerciseViewChoice.svelte
+++ src/lib/ExerciseViewChoice.svelte
@@ -7,47 +7,12 @@
   import { browser } from "$app/env"
   import type { MultipleChoiceExercise } from "./Exercise"
   import type { Base64Audio } from "./SpeechSystem"
-  import sprachy from "./sprachy"
-
-  const { effects } = sprachy.expectSPA()
+  import Choices from "./Choices.svelte"
 
   export let exercise: MultipleChoiceExercise
   export let audioPromise: Promise<Base64Audio> | undefined = undefined
-  let choicesUl: HTMLUListElement
 
-  type Choice = MultipleChoiceExercise["choices"][0]
   const dispatch = createEventDispatcher()
-
-  let chosen: Set<Choice> = new Set()
-
-  async function choose(choice: Choice) {
-    chosen = chosen.add(choice)
-    if (choice.correct) {
-      effects.confetti.spawnAt(
-        choicesUl.children[exercise.choices.indexOf(choice)] as HTMLElement
-      )
-      dispatch("correct")
-    }
-  }
-
-  function onKeydown(ev: KeyboardEvent) {
-    for (let i = 0; i < exercise.choices.length; i++) {
-      const choice = exercise.choices[i]!
-      if (ev.key === (i + 1).toString()) {
-        choose(choice)
-      }
-    }
-  }
-
-  if (browser) {
-    onMount(() => {
-      window.addEventListener("keydown", onKeydown)
-    })
-
-    onDestroy(() => {
-      window.removeEventListener("keydown", onKeydown)
-    })
-  }
 </script>
 
 <div class="exercise">
@@ -70,21 +35,7 @@
       <Sprachdown inline source={exercise.question} />
     </div>
   {/if}
-  <ul class="choices" bind:this={choicesUl}>
-    {#each exercise.choices as choice, i}
-      <li>
-        <button
-          class="btn btn-light"
-          class:incorrect={chosen.has(choice) && !choice.correct}
-          class:correct={chosen.has(choice) && choice.correct}
-          on:click={() => choose(choice)}
-        >
-          <span class="number">{i + 1}</span>
-          {choice.text}
-        </button>
-      </li>
-    {/each}
-  </ul>
+  <Choices choices={exercise.choices} />
 </div>
 
 <style>
@@ -95,25 +46,5 @@
   }
   img {
     max-height: 50vh;
-  }
-
-  .choices {
-    display: flex;
-    padding: 0;
-    margin: auto;
-  }
-
-  .choices li {
-    border: 1px solid #ccc;
-    margin-right: 0.5rem;
-    list-style: none;
-  }
-
-  .choices li button.correct {
-    background-color: #dff0d8;
-  }
-
-  .choices li button.incorrect {
-    background-color: #f2dede;
   }
 </style>
