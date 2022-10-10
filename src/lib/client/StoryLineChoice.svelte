23f52bc07e482e5c46b08ee81e2f08f09236991d
Toggle <toggledy@gmail.com>
2022-10-10 16:05:54 +1100

    Fix alignment of audio icon with text

--- src/lib/client/StoryLineChoice.svelte
+++ src/lib/client/StoryLineChoice.svelte
@@ -4,6 +4,7 @@
   import Sprachdown from "$lib/Sprachdown.svelte"
   import { createEventDispatcher } from "svelte"
   import Choices from "$lib/Choices.svelte"
+  import AudioForLine from "$lib/AudioForLine.svelte"
 
   export let line: MultipleChoiceLine
   export let complete: boolean = false
@@ -13,7 +14,11 @@
 </script>
 
 <div>
-  <Sprachdown source={line.question} />
+  <div class="question d-flex align-items-center mb-2">
+    <AudioForLine opts={{ from: "narrator", message: line.question }} />
+    <span class="me-1" />
+    <Sprachdown inline source={line.question} />
+  </div>
   <Choices
     choices={line.choices}
     on:correct={() => dispatch("correct")}
