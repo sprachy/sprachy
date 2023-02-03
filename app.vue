22d334253dacd59c99812498c7393a583af44726
Toggly <toggledy@gmail.com>
2023-02-03 19:12:30 +1100

    Add css

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import "accessible-nprogress/dist/accessible-nprogress.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "~/assets/app.css"
+</script>
+
 <template>
   <div>
     <NuxtPage />
