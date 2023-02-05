f1a36ece693ad73a2f60f213df96228aa45d8221
Toggly <toggledy@gmail.com>
2023-02-05 19:46:00 +1100

    Add patterns page

---
+++ pages/patterns.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import PatternIndex from "~/components/PatternIndex.vue"
+</script>
+
+<template>
+  <main class="container">
+    <h2>All patterns</h2>
+    <PatternIndex />
+  </main>
+</template>
