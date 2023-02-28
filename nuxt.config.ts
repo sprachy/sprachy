6ac46c2c8a6c5c7fb2ebaa21e2975a21f02ef120
Toggly <toggledy@gmail.com>
2023-03-01 01:03:20 +1100

    Add some languageUtil tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    'nuxt-vitest'
   ],
   app: {
     head: {
---
+++ test/languageUtil.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from 'vitest'
+import { getMultipleChoiceAnswerType, numToWord } from '~/lib/languageUtil'
+
+test('numToWord', () => {
+  expect(numToWord(0)).toBe('Null')
+  expect(numToWord(1)).toBe('Eins')
+  expect(numToWord(-5)).toBe('Minus Fünf')
+  expect(numToWord(21)).toBe('Einundzwanzig')
+})
+
+test('getMultipleChoiceAnswerType', () => {
+  expect(getMultipleChoiceAnswerType("Is this a fish? yes")).toBe("yes/no")
+  expect(getMultipleChoiceAnswerType("Is this a fish? no")).toBe("yes/no")
+  expect(getMultipleChoiceAnswerType("How many fish are there? 10")).toBe("numeric")
+  expect(getMultipleChoiceAnswerType("What are the people doing? laughing")).toBe("VBG")
+  expect(getMultipleChoiceAnswerType("What is in front of the fish? food")).toBe("NN")
+})
