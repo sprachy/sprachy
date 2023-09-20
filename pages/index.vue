f2484cacc0ad7b80d094afc87d1cdd7bf977cd1c
Toggly <toggledy@gmail.com>
2023-09-20 23:54:30 +1000

    Restore fancy front page somewhat

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: false
-})
-
-const currentUser = await getCurrentUser()
-const router = useRouter()
-
-if (currentUser) {
-  router.replace("/learn")
-}
+import PatternIndex from "~/components/PatternIndex.vue"
+import backgroundImg from "~/assets/sprachy-bg.webp"
 
 const pageDesc =
   "Sprachy is a German learning web app with dorky characters, linguistic patterns, SRS, and extradimensional squirrels."
+
 
 useHead({
   title: "Sprachy, a German learning application",
@@ -24,56 +17,161 @@ useHead({
 
 <template>
   <main>
-    <div>
-      <div class="title">
-        <SprachyLogo />
-        <h1>Sprachy</h1>
-        <span class="alpha">alpha</span>
+    <section class="banner" :style="{ backgroundImage: `url(${backgroundImg})` }">
+      <div class="container">
+        <div class="row">
+          <div class="signup">
+            <h1>Learn German the clever and cute way</h1>
+
+            <p>
+              Sprachy combines detailed explanations of language patterns with
+              memorable dialogue exercises. Also it involves extradimensional
+              squirrels.
+            </p>
+          </div>
+        </div>
       </div>
-      <p>An experimental web app for learning German</p>
-      <ul>
-        <li>Image-based comprehensible input</li>
-        <li>~15 hours of free exercises</li>
-        <li>Made by a native speaker and his German-learning boyfriend 🥰</li>
-      </ul>
-      <div>
-        <NuxtLink class="btn btn-success login" to="/login">Login</NuxtLink>
-        <NuxtLink class="btn btn-primary signup" to="/signup">Sign up</NuxtLink>
+    </section>
+    <!-- <section class="neat-parts">
+      <div class="container py-5">
+        <div class="d-flex justify-content-between">
+          <div class="neat-parts-card">
+            <Fa size="3x" icon={faCircleNodes} />
+            <h5>Pattern-based</h5>
+            <p>
+              Sprachy teaches the <em>patterns</em> in German, like how some noun
+              endings always imply a certain grammatical gender.
+            </p>
+          </div>
+
+          <div class="neat-parts-card">
+            <Fa size="3x" icon={faComments} />
+            <h5>Comprehensible input</h5>
+            <p>
+              Learn patterns with dialogues between cute characters, to see how
+              they're used in conversation.
+            </p>
+          </div>
+
+          <div class="neat-parts-card">
+            <Fa size="3x" icon={faBook} />
+            <h5>Clear explanations</h5>
+            <p>
+              We don't just throw sentences at you: Sprachy tells you <em>why</em> each exercise has a given answer.
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+    </section> -->
+    <section class="patterns">
+      <div class="container">
+        <h2>All patterns</h2>
+        <PatternIndex />
+      </div>
+    </section>
   </main>
 </template>
 
-<style scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  padding: 1rem;
+<style>
+.container {
+  position: relative;
 }
 
-.title {
+section.banner {
+  position: relative;
+  background-size: cover;
+  background-position: 50% 75%;
+  height: calc(100vh - 100px);
   display: flex;
   align-items: center;
+}
+
+section.banner::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+section.banner .signup {
+  width: 50%;
+  padding: 24px;
+  background: white;
+  box-shadow: 0px 3px 6px rgb(0 0 0 / 23%);
+}
+
+section.banner .signup h1 {
+  margin-top: 10px;
+  font-size: 2.8rem;
   margin-bottom: 1rem;
 }
 
-.title img {
-  margin-right: 0.5rem;
+section.banner .signup p {
+  font-size: 1.1rem;
 }
 
-.title h1 {
-  margin: 0;
-  margin-right: 0.2rem;
+section.banner .signup .form-group {
+  margin-bottom: 1rem;
 }
 
-.alpha {
-  color: green;
+section.banner .signup button {
+  width: 100%;
+  padding: 1rem;
 }
 
-a.login {
-  margin-right: 0.25rem;
+section.patterns {
+  padding-top: 3rem;
+}
+
+section.neat-parts {
+  background-color: #eee;
+}
+
+.neat-parts-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding: 1rem;
+  gap: 1rem;
+  background-color: #fafafa;
+  border-radius: 10px;
+  box-shadow: 0px 3px 6px rgb(0 0 0 / 23%);
+  flex-basis: 30%;
+  text-align: center;
+}
+
+.neat-parts-card :global(svg) {
+  margin: 1rem;
+  color: var(--sprachy-primary);
+}
+
+.neat-parts-card p {
+  color: #666;
+}
+
+@media only screen and (max-width: 992px) {
+  .neat-parts .container>div {
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+
+  .neat-parts-card {
+    flex-basis: 100%;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  section.banner {
+    padding: 2rem 1rem;
+  }
+
+  section.banner .signup {
+    width: 100%;
+  }
 }
 </style>
+
