6dc5de4ab1df42e57067c3d08a885206cecfa882
Evan Summers <tofuevan@gmail.com>
2023-10-17 22:51:16 +1100

    Switch to cloudflare_pages preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: "cloudflare_module",
+    preset: "cloudflare_pages",
   },
   content: {
     navigation: {
